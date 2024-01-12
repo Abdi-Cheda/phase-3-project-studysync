734b2c95d9719f38e071f49cad224d5b365b6c69
Abdi-Cheda <abdicheda75@gmail.com>
2024-01-12 22:59:31 +0300

    commit

--- models/courses.py
+++ models/courses.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from .base import Base
 
 class Course(Base):
     __tablename__ = 'courses'
-
     id = Column(Integer, primary_key=True)
+    name = Column(String)
+    code = Column(Integer)
+    day = Column(String)
     schedules = relationship("Schedule", back_populates="course")
