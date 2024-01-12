9ed3b6810addf68daec78824175e4187c11e3cb1
Abdi-Cheda <abdicheda75@gmail.com>
2024-01-12 23:04:54 +0300

    commit

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ Session = sessionmaker(bind=engine)
 
 @contextmanager
 def session_scope():
-    """Provide a transactional scope around a series of operations."""
     session = Session()
     try:
         yield session
