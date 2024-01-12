79806c0699b1b9681ba1f8e452703082d2e353cb
Abdi-Cheda <abdicheda75@gmail.com>
2024-01-12 23:00:10 +0300

    commit

--- models/schedule.py
+++ models/schedule.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.orm import relationship
 from .base import Base
 
 class Schedule(Base):
-    __tablename__ = 'schedules'
-
+    __tablename__ = 'schedule'
     id = Column(Integer, primary_key=True)
-    student = relationship("Student", back_populates="schedule")
+    course_id = Column(Integer, ForeignKey('courses.id'))
+    student_id = Column(Integer, ForeignKey('students.id'))
+    time = Column(String)
+    duration = Column(Integer)
+    student = relationship("Student", back_populates="schedules")
     course = relationship("Course", back_populates="schedules")
